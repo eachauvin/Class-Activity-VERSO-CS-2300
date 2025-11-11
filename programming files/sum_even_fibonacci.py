00c427f92b0deb4fe711599e0227d0beff0cb079
aemara04 <aemara@uvm.edu>
2025-11-11 10:29:12 -0500

    Refactor sum_even_fibonacci.py for clarity
    Fix syntax errors and improve comments in the Fibonacci sum calculation.

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,10 +1,13 @@
+//
 a, b = 0, 1
+//starting the sum at 0
 sum_even = 0
+//while loop
 while b < 4000000:
     if b % 2 == 0:
         sum_even += b
     a, b = b, a+b
 
 print(sum_even)
-
+#predicted answer
 # answer should be 4613732
