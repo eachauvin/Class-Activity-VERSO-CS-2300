70513818688761caaa17b01bed493d67055d95cb
NicoM2024 <142912413+NicoM2024@users.noreply.github.com>
2025-11-11 10:42:42 -0500

    Broke sum_even_fibonacci

--- programming files/sum_even_fibonacci.java
+++ programming files/sum_even_fibonacci.java
@@ -1,14 +1,16 @@
 public class sum_even_fibonacci {
     public static void main(String[] args) {
-        int a = 0, b = 1;
+        int a = 0, b = 1, c = 2;
         int sumEven = 0;
-        while (b < 4000000) {
-            if (b % 2 == 0) {
-                sumEven += b;
+        while (c < 4000000) {
+            if (c % 2 == 0) {
+                sumEven += c;
             }
-            int temp = b;
-            b = a + b;
-            a = temp;
+            int temp2 = c;
+            int temp1 = b;
+            c = a + b + c;
+            a = temp1;
+            b = temp2;
         }
         System.out.println(sumEven);
     }
