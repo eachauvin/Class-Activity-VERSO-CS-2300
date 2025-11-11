c5d5881a3e1094d8a87cd6cdf05633fee524bfac
eachauvin <eachauvin@gmail.com>
2025-11-11 10:32:09 -0500

    Update datetime_convert.java

--- programming files/datetime_convert.java
+++ programming files/datetime_convert.java
@@ -4,8 +4,14 @@ import java.time.format.DateTimeFormatter;
 public class datetime_convert {
     public static void main(String[] args) {
         String dateStr = "2022-03-17 10:45:30";
+
+        //Formats the string into year, month, date, hour, minute, second
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        //sets the dateObj to the current date and time
         LocalDateTime dateObj = LocalDateTime.parse(dateStr, formatter);
+
+        //formats the date into the proper format
         String formattedDate = dateObj.format(DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss"));
 
         System.out.println(formattedDate);
