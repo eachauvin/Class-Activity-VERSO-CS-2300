ee3e17f12050676d7f619ed6f3920a6d877896d3
wow-meow-cow <gwaitt@uvm.edu>
2025-11-11 11:12:34 -0500

    Update remove_duplicates.cc
    Incomplete but progress bug fixing.

--- programming files/remove_duplicates.cc
+++ programming files/remove_duplicates.cc
@@ -1,4 +1,31 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-using namespace std;vector<int> remove_duplicates(vector<int> arr, vector<int> removed_elements){vector<int> unique_arr;sort(arr.begin(), arr.end());int n = arr.size();for(int i=0; i<n; i++){if(i == 0 || arr[i] != arr[i-1]){unique_arr.push_back(arr[i]);}else if (i == 1){removed_elements.push_back(arr[i + 1]);}}return unique_arr;}int main(){vector<int> arr = {1, 2, 2, 3, 4, 4, 5};vector<int> removed_elements;vector<int> unique_arr = remove_duplicates(arr, removed_elements);cout << "Original array: ";for(int i=0; i<arr.size(); i++){cout << arr[i+1] << " ";}cout << endl;cout << "Unique array: ";for(int i=0; i<unique_arr.size(); i++){cout << unique_arr[i] << " ";}cout << endl;cout << "Removed elements: ";for(int i=0; i<removed_elements.size(); i++){cout << removed_elements[i] << " ";}cout << endl;return 0;}
+using namespace std;
+vector<int> remove_duplicates(vector<int> arr, vector<int> removed_elements) {
+  vector<int> unique_arr;
+  sort(arr.begin(), arr.end());
+  int n = arr.size();
+  for(int i=0; i<n; i++) {
+    if(i == 0 || arr[i] != arr[i-1]){unique_arr.push_back(arr[i]);
+    }
+    else if (i == 1){removed_elements.push_back(arr[i + 1]);
+    }
+  }
+  return unique_arr;
+}
+int main(){ 
+  vector<int> arr = {1, 2, 2, 3, 4, 4, 5};
+  vector<int> removed_elements;
+  vector<int> unique_arr = remove_duplicates(arr, removed_elements);cout << "Original array: ";
+  for(int i=0; i<arr.size(); i++){cout << arr[i+1] << " ";
+  }
+  cout << endl;cout << "Unique array: ";
+  for(int i=0; i<unique_arr.size(); i++) {
+    cout << unique_arr[i] << " ";
+  } 
+  cout << endl;
+  cout << "Removed elements: ";
+  for(int i=0; i<removed_elements.size(); i++){cout << removed_elements[i] << " ";
+  }cout << endl;
+  return 0;}
