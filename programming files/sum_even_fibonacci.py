517a437940b45f26f06d02b975bea18e1cc13774
ellpow <156821132+ellpow@users.noreply.github.com>
2025-11-11 10:50:46 -0500

    Update sum_even_fibonacci.py
    Elliot: created 4 bugs and documentation

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,10 +1,14 @@
-a, b = 0, 1
+# Should return sum of all even numbers below 4000000
+# Elliot Powell changed this code
+# Bugs: 4 
+# 3 logic errors amd 1 compiler error
+a, b = 1, 1
 sum_even = 0
 while b < 4000000:
-    if b % 2 == 0:
-        sum_even += b
+    if b % 2 = 0:
+        sum_even = b
     a, b = b, a+b
 
-print(sum_even)
+print(b)
 
 # answer should be 4613732
