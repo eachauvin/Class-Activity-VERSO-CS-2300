ddaaa3f2db950adb3f48dc35889697fbbb781d3c
Daedalus99 <62578295+Daedalus99@users.noreply.github.com>
2025-11-11 10:42:36 -0500

    Update rgb_to_hex.java

--- programming files/rgb_to_hex.java
+++ programming files/rgb_to_hex.java
@@ -8,9 +8,9 @@ public class rgb_to_hex {
     }
 
     public static String rgbToHex(int r, int g, int b) {
-        r = Math.min(255, Math.max(0, r));
-        g = Math.min(255, Math.max(0, g));
-        b = Math.min(255, Math.max(0, b));
+        r = 255 - Math.min(255, Math.max(0, r));
+        g = 255 - Math.min(255, Math.max(0, g));
+        b = 255 - Math.min(255, Math.max(0, b));
         return String.format("%02X%02X%02X", r, g, b);
     }
 }
