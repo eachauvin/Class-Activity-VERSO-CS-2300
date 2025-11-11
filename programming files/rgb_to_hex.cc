3ce25e1660b56c19d8de6ef22d071939384658d8
wow-meow-cow <gwaitt@uvm.edu>
2025-11-11 10:30:55 -0500

    Update rgb_to_hex.cc
    GW

--- programming files/rgb_to_hex.cc
+++ programming files/rgb_to_hex.cc
@@ -1,17 +1,23 @@
+// GW
 #include <iostream>
 #include <sstream>
 #include <iomanip>
 
+// A function that takes in 3 integer value 
 std::string rgb_to_hex(int r, int g, int b)
 {
+    // Ensuring our value remains between 0 and 255..?
     r = std::max(0, std::min(255, r));
     g = std::max(0, std::min(255, g));
     b = std::max(0, std::min(255, b));
 
     std::stringstream ss;
+
+    // Converts values to hex
     ss << std::uppercase << std::hex << std::setfill('0')
        << std::setw(2) << r << std::setw(2) << g << std::setw(2) << b;
 
+    // Returns the hex string
     return ss.str();
 }
 
